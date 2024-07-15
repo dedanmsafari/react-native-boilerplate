250ffc7ca069eff814aa24f0298a9baa0b9f4d05
dedanmsafari <dedan.msafari@gmail.com>
2024-07-15 06:12:58 +0300

    refactor: final commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View
       style={{
+        alignItems: 'center',
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+        justifyContent: 'center',
+      }}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
